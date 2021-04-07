0a8415f5580a03b52ebc4c38e0be58d812f7fdba
Erik Schierboom <erik_schierboom@hotmail.com>
2021-04-07 15:01:24 +0200

    Run without network and as read-only file system in Docker (#26)

--- bin/run-tests-in-docker.sh
+++ bin/run-tests-in-docker.sh
@@ -4,6 +4,9 @@
 docker build --rm --no-cache -t generic-test-runner .
 
 docker run \
+    --network none \
+    --read-only \
     --mount type=bind,src=$PWD/$testdir,dst=/solution \
     --mount type=bind,src=$PWD/test/output/,dst=/output \
+    --mount type=tmpfs,dst=/tmp \
     generic-test-runner --entrypoint /opts/test-runner/bin/run-all.sh /solution/
