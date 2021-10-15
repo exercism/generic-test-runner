9fe1c2c0cc830ef2c6c3647e535385505dabb873
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-15 11:53:01 +0200

    Allow relative paths

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -6,25 +6,25 @@
 
 # Arguments:
 # $1: exercise slug
-# $2: absolute path to solution folder
-# $3: absolute path to output directory
+# $2: path to solution folder
+# $3: path to output directory
 
 # Output:
 # Writes the test results to a results.json file in the passed-in output directory.
 # The test results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
 
 # Example:
-# ./bin/run-in-docker.sh two-fer /absolute/path/to/two-fer/solution/folder/ /absolute/path/to/output/directory/
+# ./bin/run-in-docker.sh two-fer path/to/solution/folder/ path/to/output/directory/
 
 # If any required arguments is missing, print the usage and exit
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "usage: ./bin/run-in-docker.sh exercise-slug /absolute/path/to/solution/folder/ /absolute/path/to/output/directory/"
+    echo "usage: ./bin/run-in-docker.sh exercise-slug path/to/solution/folder/ path/to/output/directory/"
     exit 1
 fi
 
 slug="$1"
-input_dir="${2%/}"
-output_dir="${3%/}"
+solution_dir=$(realpath "${2%/}")
+output_dir=$(realpath "${3%/}")
 
 # Create the output directory if it doesn't exist
 mkdir -p "${output_dir}"
@@ -37,7 +37,7 @@ docker run \
     --rm \
     --network none \
     --read-only \
-    --mount type=bind,src="${input_dir}",dst=/solution \
+    --mount type=bind,src="${solution_dir}",dst=/solution \
     --mount type=bind,src="${output_dir}",dst=/output \
     --mount type=tmpfs,dst=/tmp \
     exercism/test-runner "${slug}" /solution /output 
