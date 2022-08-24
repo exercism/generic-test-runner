d45800b051683b8d7574db200d1e458f5d4b1c8b
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-24 14:11:38 +0200

    Stop executing when a command returns a non-zero exit code (#79)

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -16,6 +16,9 @@
 # Example:
 # ./bin/run-in-docker.sh two-fer path/to/solution/folder/ path/to/output/directory/
 
+# Stop executing when a command returns a non-zero return code
+set -e
+
 # If any required arguments is missing, print the usage and exit
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     echo "usage: ./bin/run-in-docker.sh exercise-slug path/to/solution/folder/ path/to/output/directory/"
