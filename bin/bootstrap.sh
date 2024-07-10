49d1b0519540ea5768bd5d27d9f605a07c213528
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-10 16:29:55 +0200

    Add more stuff

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -12,8 +12,20 @@ if [ -z "${LANGUAGE}" ] || [ -z "${SLUG}" ]; then
     exit 1
 fi
 
-REPO="exercism/${SLUG}-test-runner"
-gh repo create --public --template https://github.com/exercism/generic-test-runner/ "${REPO}"
+ORG="exercism"
+REPO="${ORG}/${SLUG}-test-runner"
+gh repo create --public --template https://github.com/${ORG}/generic-test-runner/ "${REPO}"
+
+# Disable merge commits and rebase merges
 gh api --method PATCH "/repos/${REPO}" -f allow_merge_commit=false -f allow_rebase_merge=false
-gh api --method PUT "/orgs/exercism/teams/maintainers-admin/repos/${REPO}" -f "permission=maintain"
-gh api --method PUT "/orgs/exercism/teams/${SLUG}/repos/${REPO}" -f "permission=push"
+
+# Update team permissions
+gh api --method PUT "/orgs/${ORG}/teams/maintainers-admin/repos/${REPO}" -f "permission=maintain"
+gh api --method PUT "/orgs/${ORG}/teams/${SLUG}/repos/${REPO}" -f "permission=push"
+
+# Add repo to deploy secrets
+REPO_ID=$(gh api "repos/${REPO}" --jq '.id')
+gh api --method PUT "/orgs/${ORG}/actions/secrets/AWS_ECR_ACCESS_KEY_ID/repositories/${REPO_ID}"
+gh api --method PUT "/orgs/${ORG}/actions/secrets/AWS_ECR_SECRET_ACCESS_KEY/repositories/${REPO_ID}"
+gh api --method PUT "/orgs/${ORG}/actions/secrets/DOCKERHUB_PASSWORD/repositories/${REPO_ID}"
+gh api --method PUT "/orgs/${ORG}/actions/secrets/DOCKERHUB_USERNAME/repositories/${REPO_ID}"
